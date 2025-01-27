8fe402c53684e4d1ccb51af2654cc3fc2eaac295
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2025-01-28 00:29:34 +0530

    validation added for signup route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { body, query, validationResult } = require('express-validator')
 
-const { getUsers, createUsers, loginUser, deleteUser, updateUser } = require('../controllers/userController')
+const { createUsers, loginUser, deleteUser, updateUser } = require('../controllers/userController')
 
-router.post('/signup', createUsers)
+router.post('/signup', [
+    body('username').trim().isAlpha().isLength({ min: 6 }).withMessage("username must be atleast 6 characters long"),
+    body('email').trim().isEmail().withMessage("email is not valid"),
+    body('password').trim().isLength({ min: 6 }).withMessage("password must atleast be 6 char long")
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const errArray = errors.array();
+        return res.status(400).json({ message: errArray[0].msg })
+    }
+    await createUsers(req, res);
+})
 
 router.delete('/delete', deleteUser)
 
