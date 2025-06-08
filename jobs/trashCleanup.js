06fca8e31576d3a4fe239d97c5e2d4768290a5ac
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2025-06-09 02:16:39 +0530

    auto-deleting works :0

--- jobs/trashCleanup.js
+++ jobs/trashCleanup.js
@@ -1,11 +1,12 @@
 const cron = require('node-cron')
-const { pool } = require('../config/db')
+const pool = require('../config/db')
 
 const trashCleanup = () => {
     cron.schedule('*/2 * * * *', async () => {
         try {
             console.log("🧹 Running trash cleanup job...");
-            await pool.query(`delete from trash where created_at <= NOW() - INTERVAL '2 minutes'`);
+            const query = `delete from trash where created_at <= NOW() - INTERVAL '2 minutes'`
+            await pool.query(query);
         } catch (err) {
             console.error("Cron job failed:", err);
         }
